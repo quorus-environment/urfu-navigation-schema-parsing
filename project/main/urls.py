18bef8fdde27183e67ca3d0e152b40bd447ff696
Ivan Knigencev <74624665+IKnigencev@users.noreply.github.com>
2023-03-16 19:11:50 +0500

    flake8

--- project/main/urls.py
+++ project/main/urls.py
@@ -8,7 +8,6 @@ app_name = 'api'
 
 # router_v1 = DefaultRouter()
 # router_v1.register("offices", OfficeViews, basename="offices")
-
 urlpatterns = [
     path('v1/save_point', ImageFrontView.as_view(), name='save_point'),
     # path('v1/', include(router_v1.urls))
